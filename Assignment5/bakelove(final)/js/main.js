caeb3e75875791318e21b0f9f34f7ecb8c0ea12c
Ritika Bhagya <ritika.bhagya@gmail.com>
2013-10-19 23:07:52 -0400

    fixed javascript scroll animation

--- Assignment5/bakelove(final)/js/main.js
+++ Assignment5/bakelove(final)/js/main.js
@@ -38,18 +38,18 @@ function menuShows(){
 	}
 }
 
-$(function () {
+// $(function () {
  
-$('a[href^="#"]').click(function(event) {
-var id = $(this).attr("href");
-var offset = 20;
-var target = $(id).offset().top - offset;
+// $('a[href^="#"]').click(function(event) {
+// var id = $(this).attr("href");
+// var offset = 20;
+// var target = $(id).offset().top - offset;
  
-$('html, body').animate({scrollTop:target}, 500);
-event.preventDefault();
-});
+// $('html, body').animate({scrollTop:target}, 500);
+// event.preventDefault();
+// });
  
-});
+// });
 
 
 
